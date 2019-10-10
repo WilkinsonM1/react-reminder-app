844e96b4d06dab97d73493cbdda95674b3878574
CN72 <cn72@CN72.local>
2019-10-10 10:45:20 +0100

    Updates

---
+++ precache-manifest.7a6314a9c027f117b9c78bc5aa27e79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00b882e553f581c0fe97b08ee6014f0",
+    "url": "/react-reminder-app/index.html"
+  },
+  {
+    "revision": "caf6b960ab8c85b5e84f",
+    "url": "/react-reminder-app/static/css/main.6a496fbf.chunk.css"
+  },
+  {
+    "revision": "82c19241cf3af8a6d9a2",
+    "url": "/react-reminder-app/static/js/2.e13a810d.chunk.js"
+  },
+  {
+    "revision": "caf6b960ab8c85b5e84f",
+    "url": "/react-reminder-app/static/js/main.f0f8745f.chunk.js"
+  },
+  {
+    "revision": "b81154305211e4920922",
+    "url": "/react-reminder-app/static/js/runtime-main.52aaae0a.js"
+  }
+]);
